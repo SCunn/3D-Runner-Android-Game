d6da2447ad1bb522557a3bd172047770b7f00700
SCunn <43576829+SCunn@users.noreply.github.com>
2024-08-28 14:27:28 +0100

    scoring system and start game add, app completed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,12 +9,16 @@ public class GameManager : MonoBehaviour
     public GameObject obstacle;
     public Transform spawnPoint;
     int score = 0;
-    
+
+    public TextMeshProUGUI scoreText;
+    public GameObject playButton;
+    public GameObject player;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        GameStart();
+        
     }
 
     // Update is called once per frame
@@ -38,8 +43,18 @@ public class GameManager : MonoBehaviour
         
     }
 
+    void ScoreUp()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
     public void GameStart()
     {
+        player.SetActive(true);
+        playButton.SetActive(false);
+
         StartCoroutine("SpawnObstacle");
+        InvokeRepeating("ScoreUp", 2f, 1f);
     }
 }
